ab6688c3e5ae2ffc2a5fca67313d21103e6f4123
Omer Schwartz <knowone.omer@gmail.com>
2016-11-29 10:16:30 +0200

    Added Empty project files Changed current files to match Windows env

---
+++ ex2/ex2.cpp
@@ -0,0 +1,416 @@
+
+/**
+* File: ex2.c
+Double Soduku solver
+====================
+Written by: Omer Schwartz - id: 201234002, login: omersc and
+Matan Bachar -  id: 203167333, login: matanba
+
+This program receives data from the user, as sets of 3 integers representing the Soduku board
+he wants to solve.
+The first integer represents the row number of the cell he wants to fill, the second integer
+represents the column and the third integer is the content of that certain cell.
+To finish the input the user will be required to write -1.
+
+The program will create two processes that will solve the Soduku board in two different ways.
+The first process will solve the board as we solved it last year, in a simple backtracking recursion.
+The second child will solve the board while using a priority (markup) table that updates and chooses
+for each iteration the highest priority cell.
+
+As a result, the program will print the solved Soduko board 2 times, each process will print his solution,
+and then each process will print it's runtime (in seconds) to get to the solution.
+*/
+
+
+//============================================================================//
+//                             Include Section
+//============================================================================//
+
+#include "ex2.h"
+
+//============================================================================//
+//                           Function Implementation
+//============================================================================//
+
+/****************************************************************************
+* Checks if the value of digit is allowed in the current square of the
+* associated cell positioned in (currRow, currCol).
+* Returns true if no cell in the associated square is equal to digit.
+* False, otherwise.
+*
+* *************************************************************************/
+bool isAllowSqr(Board* board, int currRow, int currCol, int digit) {
+
+	bool found = false;
+	int i, j, startRowPos, startColPos;
+	startRowPos = (currRow / N)*N;            //get the first cell of the relevant square
+	startColPos = (currCol / N)*N;            //if N=3, then we get either 0*3=0, 1*3=3, or 2*3=6
+											  //which are the starting position of the square (row&col)
+
+	for (i = startRowPos; i < startRowPos + N; i++) {
+		for (j = startColPos; j < startColPos + N; j++) {
+			if (board->_board[i][j]._content == digit) {     //check all cells in the square
+				return found;                               //return when found
+			}
+		}
+	}
+	return !found;                                          //return not found (isAllowed=true)
+}
+
+
+/***************************************************************************
+* Check if the value of digit is allowed in the current column and row of
+* the cell, positioned in (currRow, currCol).
+* Returns true if no cell in the associated row or column is equal to digit.
+* False if otherwise.
+* *************************************************************************/
+bool isAllowRowCol(Board* board, int currRow, int currCol, int digit) {
+	bool found = false;
+	int i;
+	for (i = 0; i < DIGITS; i++) {         //go across the row and column to look for digit
+		if (board->_board[currRow][i]._content == digit || board->_board[i][currCol]._content == digit) {
+			return found;
+		}
+	}
+	return !(found);
+}
+
+
+bool isAllowed(Board* board, int currRow, int currCol, int digit) {
+	//true if didn't find in the square and didn't find in the row and column
+	return isAllowSqr(board, currRow, currCol, digit) && isAllowRowCol(board, currRow, currCol, digit);
+}
+
+
+
+void printBoard(Board* board) {
+	int i, j;
+	for (i = 0; i < DIGITS; ++i) {
+		for (j = 0; j < DIGITS; ++j) {
+			//print line
+			printf("%d ", board->_board[i][j]._content);
+		}                               //separate the lines with newline
+		printf("\n");
+	}
+	putchar('\n');                      //flush the buffer, end with newline
+}
+
+
+Board copyBoard(Board* b) {
+	Board newBoard = {};                //create empty struct
+	int i, j, k;
+	for (i = 0; i < DIGITS; ++i) {
+		for (j = 0; j < DIGITS; ++j) {
+			//copy the cell's content and priority
+			newBoard._board[i][j]._content = b->_board[i][j]._content;
+			newBoard._board[i][j]._priority = b->_board[i][j]._priority;
+			for (k = 0; k < DIGITS; ++k) {
+				//copy each cell's markup table
+				newBoard._board[i][j]._markupTable[k] = b->_board[i][j]._markupTable[k];
+			}
+
+		}
+	}
+	return newBoard;
+}
+
+Board getBoardFromUser() {
+
+	int i, j, k;
+	Board board = {};                               //Create empty struct
+	for (i = 0; i < DIGITS; ++i) {              //"Zero-out" the Board
+		for (j = 0; j < DIGITS; ++j) {
+			board._board[i][j]._content = 0;
+			for (k = 0; k < DIGITS; ++k) {
+				board._board[i][j]._markupTable[k] = POSSIBLE;  //make all cells possible
+			}
+			board._board[i][j]._priority = DIGITS;  //all cells have minimum priority
+		}
+	}
+	int rowPos, colPos, digit;
+	int input;
+	bool stop = false;
+	printf("Input your data\n");
+	while (!stop) {                  //user has input
+		if (scanf("%d", &input)>0) {
+			if (input == -1) {       //indicating user stopped entering sets-of-3
+				stop = true;
+			}
+			else {                   //get next set-of-3 from user
+				rowPos = input;
+				if (scanf("%d", &colPos)) {
+					stop = true;
+				}
+				if (scanf("%d", &digit)) {
+					stop = true;
+				}
+				board._board[rowPos][colPos]._content = digit;
+				board._board[rowPos][colPos]._priority = 0;     //Cell is in use. Has no priority.
+				updateMarkup(&board, rowPos, colPos, digit);    //Update all relevant cells affected by the cell's
+																//content.
+			}
+		}
+	}
+
+	return board;
+}
+
+bool updateMarkup(Board* board, int row, int col, int digit) {
+	bool result = SUCCESS;
+	int i;
+	/* **********************************************************************************************
+	* All three blocks has the same logic:
+	* if the cell is empty, we check if the desired
+	* input (digit, that we want to put in, but still didn't)
+	* will not violate the following cells (in the row, the column
+	* and the square).
+	* If it is not allowed, AND it was not previously marked impossible
+	* then we mark that cell as impossible (in it's markup table).
+	* If it is allowed, AND it was marked as impossible (due to old attempts)
+	* then we mark that cell as possible.
+	* When we take out a possibility we check if we got to a dead end
+	* by checking if that (empty) cell's priority is 0, meaning
+	* there are no more options for a cell content and this is indeed
+	* a dead end.
+	* ***********************************************************************************************/
+	for (i = 0; i < DIGITS; i++) {
+		if (board->_board[row][i]._content == 0) {
+			//checking the cell's associated row cells
+			if (!isAllowed(board, row, i, digit)) {
+				if (board->_board[row][i]._markupTable[digit - 1] != IMPOSSIBLE) {
+					board->_board[row][i]._markupTable[digit - 1] = IMPOSSIBLE;
+					board->_board[row][i]._priority--;
+					if (board->_board[row][i]._priority == 0) {
+						result = FAILED;
+					}
+				}
+			}
+			else {
+				if (board->_board[row][i]._markupTable[digit - 1] != POSSIBLE) {
+					board->_board[row][i]._markupTable[digit - 1] = POSSIBLE;
+					board->_board[row][i]._priority++;
+				}
+			}
+		}
+		//checking the cell's associated column cells
+		if (board->_board[i][col]._content == 0) {
+			if (!isAllowed(board, i, col, digit)) {
+				if (board->_board[i][col]._markupTable[digit - 1] != IMPOSSIBLE) {
+					board->_board[i][col]._markupTable[digit - 1] = IMPOSSIBLE;
+					board->_board[i][col]._priority--;
+					if (board->_board[i][col]._priority == 0) {
+						result = FAILED;
+					}
+				}
+			}
+			else {
+				if (board->_board[i][col]._markupTable[digit - 1] != POSSIBLE) {
+					board->_board[i][col]._markupTable[digit - 1] = POSSIBLE;
+					board->_board[i][col]._priority++;
+				}
+			}
+		}
+	}
+	//same technique used in isAllowed to check the cell's associated square
+	int j, startRowPos, startColPos;
+	startRowPos = (row / N)*N;
+	startColPos = (col / N)*N;
+
+	for (i = startRowPos; i < startRowPos + N; i++) {
+		for (j = startColPos; j < startColPos + N; j++) {
+			if (board->_board[i][j]._content == 0) {
+				if (!isAllowed(board, i, j, digit)) {
+					if (board->_board[i][j]._markupTable[digit - 1] != IMPOSSIBLE) {
+						board->_board[i][j]._markupTable[digit - 1] = IMPOSSIBLE;
+						board->_board[i][j]._priority--;
+						if (board->_board[i][j]._priority == 0) {
+							result = FAILED;
+						}
+					}
+				}
+				else {
+					if (board->_board[i][j]._markupTable[digit - 1] != POSSIBLE) {
+						board->_board[i][j]._markupTable[digit - 1] = POSSIBLE;
+						board->_board[i][j]._priority++;
+					}
+				}
+			}
+		}
+	}
+	return result;  //false if found a cell without options
+}
+
+bool findHighestPriority(Board* board, int* row, int* col) {
+
+	int minPriority = DIGITS + 1;        //set to the lowest priority (the lower the better)
+	int i, j;
+	for (i = 0; i < DIGITS; ++i) {
+		for (j = 0; j < DIGITS; ++j) {
+			if (board->_board[i][j]._content == 0) {     //if cell is empty
+
+														 //if current cell has a lower priority than the previous cell
+				if (board->_board[i][j]._priority < minPriority && board->_board[i][j]._priority > 0) {
+					minPriority = board->_board[i][j]._priority;    //replace the minimum
+					*row = i;                                       //update the newly-found cell's location
+					*col = j;                                       //(row,col)
+				}
+				if (minPriority == 1) {                              //immediately stop if found an only-1-option cell
+					return SUCCESS;
+				}
+			}
+		}
+	}
+	if (minPriority == DIGITS + 1) {  //no more cells are left to fulfill - meaning the board is finished
+		return FINISHED_BOARD;              //this is equals to false
+	}
+	else {
+		return SUCCESS;
+	}
+
+}
+
+bool solveA(Board *board, int row, int col) {
+	/* Root condition - successfully ending the algorithm.
+	* When all the cells of the board have been handled*/
+	if (col == DIGITS && row == DIGITS - 1) {  //row is the last row in array, col the the one after the last
+											   //indicating there are no more cells left
+		return SUCCESS;
+	}
+	int i;
+	bool result;
+
+	/* If there is no value assigned to the cell: */
+	if (row < DIGITS && col < DIGITS && board->_board[row][col]._content == 0) {
+
+		/* Try for each digit all combinations from the next column  */
+		for (i = 1; i <= DIGITS; i++) {
+			if (isAllowed(board, row, col, i)) { //isAllowed checks for column, row and square assignment options
+				board->_board[row][col]._content = i;   //assign the value if allowed
+				result = solveA(board, row, col + 1);   //go to next column
+				if (result) {                        //If successful then a solution was found
+					return SUCCESS;
+				}
+			}
+		}
+
+		/* If all digits are not allowed or do not lead to solution,
+		* zero the assigned cell and return FAILED                     */
+		board->_board[row][col]._content = 0;
+		return FAILED;
+	}
+	else {                                   //Non zero cell is reached
+		if (col >= DIGITS) {
+			return solveA(board, row + 1, 0);   //Go to next row if column is last column
+		}
+		return solveA(board, row, col + 1);     //otherwise go to next column
+	}
+}
+
+bool solveB(Board* board, int row, int col) {
+
+	bool result;
+	int i;
+	/* Root condition - no more cells to fulfill
+	* If all cells have 0 priority and non empty content */
+	if (!findHighestPriority(board, &row, &col)) {
+		//updates to the new row and col and if returns false we finished the board
+		return SUCCESS;
+	}
+
+	//Run through the high-priority cell's markup table (option list)
+	for (i = 1; i <= DIGITS; ++i) {
+		//ignore all impossible values
+		if (board->_board[row][col]._markupTable[i - 1] == POSSIBLE) {
+			//check if digit i is allowed with the other associated cells
+			if (isAllowed(board, row, col, i)) {
+				//if allowed, update the associated cells markup table (option list)
+				if (updateMarkup(board, row, col, i)) {
+					//set the cell's content
+					board->_board[row][col]._content = i;
+					//Try to fulfill the next high-priority cell.
+					result = solveB(board, row, col);
+					//If unsuccessful reset what was done and try next markup option
+					if (result) {
+						//if successfull - a solution was found - return true
+						return SUCCESS;
+					}
+					else {
+
+						board->_board[row][col]._content = 0;
+						updateMarkup(board, row, col, i);
+
+					}
+				}
+			}
+		}
+	}
+	//No solution path was found, return back
+	return FAILED;
+}
+
+
+bool startChild(Board* b, SOLUTION s) {
+	bool result;
+	time_t start = time(NULL);      //Get the initial starting time
+	switch (s) {
+	case SOLUTION_A:            //First simple backtracking implementation
+		result = solveA(b, 0, 0);
+		break;
+	case SOLUTION_B:            //Second markup backtracking implementation
+		result = solveB(b, 0, 0);
+		break;
+
+	default:                   //case a wrong selection was made
+		printf("No selection was made");
+		result = false;
+		break;
+	}
+	time_t end = time(NULL);        //Get the finishing time
+	time_t lapse = end - start;     //Calculates the elapsed time
+									//print to user the running time
+	printf("Child %d took %f seconds to run\n", s + 1, (double)lapse);
+	return result;
+}
+
+int main() {
+	int i;
+	Board b1 = getBoardFromUser();  //Create a user with user input
+	Board b2 = copyBoard(&b1);      //Create a hard-copy of the board for second process
+	int status[2];                //Store both children PID
+	status[0] = _spawnl();             //create first process
+	if (status[0] == 0) {
+		//first child continues here
+		if (startChild(&b1, SOLUTION_A)) {
+			printf("Child %d solution:\n", SOLUTION_A + 1);
+			printBoard(&b1);        //print the solution board
+		}
+		else {
+			printf("Failed creating first child process!\n");
+			return -1;
+		}
+
+	}
+	else {                           //Parent process continues
+		status[1] = fork();         //creates second child process
+		if (status[1] == 0) {
+			//second child continues here
+			if (startChild(&b2, SOLUTION_B)) {
+				printf("Child %d solution:\n", SOLUTION_B + 1);
+				printBoard(&b2);    //print the solution board
+			}
+			else {
+				printf("Failed creating second child process!\n");
+				return -1;
+			}
+		}
+		else {
+			//parent process continues
+			for (i = 0; i <= 1; ++i) {      //Wait for both children to finish running.
+				waitpid(status[i], NULL, 0);
+			}
+			printf("Enjoy your solutions!\n"); //print out a good-bye msg
+		}
+	}
+
+	return 0;
+}
---
+++ ex2/ex2.h
@@ -0,0 +1,197 @@
+#pragma once
+/**
+* File: ex2.h
+Double Soduku solver
+====================
+Written by: Omer Schwartz - id: 201234002, login: omersc and
+Matan Bachar -  id: 203167333, login: matanba
+*/
+
+#ifndef EX2_EX2_H
+#define EX2_EX2_H
+
+#endif //EX2_EX2_H
+
+//============================================================================//
+//                             Include Section
+//============================================================================//
+#include <Windows.h>
+#include <process.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+//============================================================================//
+//                             Define Section
+//============================================================================//
+
+#define N 3             //The size of root (Complexity is an exponential N^3)
+#define DIGITS N*N      //How many digits are allowed or the size of each row
+//in the board
+#define FAILED false
+#define SUCCESS true
+#define POSSIBLE 1
+#define IMPOSSIBLE 0
+#define FINISHED_BOARD false
+
+/******************************************************************************
+* Solution enum for startChild - each solution is a different implementation
+* of a Sudoku solving algorithm.
+*******************************************************************************/
+typedef enum { SOLUTION_A, SOLUTION_B } SOLUTION;
+
+/******************************************************************************
+* Defining a struct to represent the content of a cell, made up of content and the
+* markup table to keep the available cells in, and the priority indicator
+*****************************************************************************/
+typedef struct {
+	int _content;
+	int _priority;
+	int _markupTable[DIGITS];
+} Cell;
+
+/*****************************************************************************
+* Defining a board that represents an array of arrays of Cell struct to
+* represent the Sudoku board.
+*****************************************************************************/
+typedef struct {
+	Cell _board[DIGITS][DIGITS];
+} Board;
+
+
+//============================================================================//
+//                           Function Declaration
+//============================================================================//
+
+/**************************************************************************
+* isAllowed checks if no Sudoku rule is violated when inserting the
+* value of @param digit into the cell at (currRow, currCol) in the
+* current Sudoku board.
+*
+* @param board the Board struct containing a legal DIGITSxDIGITS size board
+* @param currRow the target cell's row position (0..DIGITS-1)
+* @param currCol the target cell's column position (0..DIGITS-1)
+* The position values are not checked and may throw an error
+* @param digit the value to be checked if allowd in position (currRow, currCol).
+*
+* @return true if digit is an allowed move, or false if digit will violate
+* a Sudoku rule.
+* *************************************************************************/
+bool isAllowed(Board* board, int currRow, int currCol, int digit);
+
+
+/**************************************************************************
+* printBoard - simply prints the Sudoku game board.
+* @param board the Board struct to print.
+*
+* *************************************************************************/
+void printBoard(Board* board);
+
+
+/**************************************************************************
+* copyBoard returns a copy of the given board.
+* Copy-by-value each of the struct's fields and returns the copied struct.
+*
+* @param b a Board struct to by copied by value.
+* @return a copy of the original Board struct.
+* *************************************************************************/
+Board copyBoard(Board* b);
+
+
+/***************************************************************************
+* getBoardFromUser receives a valid Sudoku board details and creates a valid Board with the
+* relevant fields.
+* User inputs a row, a column and a cell value, three numbers at a time for
+* each cell. Once done the user inputs to stdin -1 to indicate the suer was
+* finished.
+* Function returns a Board struct after it had calculated the markup for the
+* input cells and the relevnat other cells and zeroed all other cells.
+* @return Board struct with user input.
+****************************************************************************/
+Board getBoardFromUser();
+
+
+/*****************************************************************************
+* updateMarkup calculates the markup for the cells associated with the cell at position
+* (row, col) with the value of digit.
+* Markup is the possible values a valid sudoku cell can have. If digit is
+* put to the cell, the associated row, column and square cannot contain the
+* same digit, and thus removed from the markup table.
+* @param board the currently built sudoku board
+* @param row the digit's row position.
+* @param col the digit's column position
+* @return true if the update returned a valid board, or false if the update
+* caused the board to become invalid - meaning a cell has no digit and no
+* options to insert a digit to it.
+*****************************************************************************/
+bool updateMarkup(Board* board, int row, int col, int digit);
+
+
+/*****************************************************************************
+* findHighestPriority searches the board for a cell with the highest priority and update the values
+* of @param row and @param col (using pointer dereference) to the current
+* position of that cell.
+* A priority is considered high when the cell has the LEAST possible options
+* to accept a digit, thus is more likely that option is the correct option.
+* The priority counter counts the possibilities a cell can have, meaning
+* Lower is Better.
+*
+* @param board the board pointer to search it's cells.
+* @param row the position reference that will be changed to the highest
+* priority cell's row position.
+* @param col the position reference that will be changed to the highest
+* priority cell's column position.
+*
+* @return true if a high priority cell was found.
+*  false if no cell with priority was found. Returning false means there are
+*  no options left to position a digit in the board, which in turn means the
+*  board is either complete or is wrong.
+*  In our program, if false is returned the board is (necessarily) solved.
+*****************************************************************************/
+bool findHighestPriority(Board* board, int* row, int* col);
+
+
+/******************************************************************************
+* SolveA uses a backtracking algorithm to iterate over all possible solutions
+* to the Sudoku board and stops when any solution was reached.
+* The board is filled with the correct solution and the boolean true value is
+* returned to indicate a solution was found.
+* Otherwise, the board stays untouched and a false value is returned.
+*
+* @param board a partial or empty Board Struct to find its solution
+* @param row the starting row position - used for the recursion. Must begin with 0.
+* @param col the starting column position, same as row. Must begin with 0.
+*
+* @return true if solution was found. False otherwise.
+*
+*****************************************************************************/
+bool solveA(Board *board, int row, int col);
+
+
+/*****************************************************************************
+* SolveB uses a markup table to track the current possibilities for each cell
+* on the board, and each iteration chooses to solve the cell with the least
+* amount of options, and also skips the unavailable options for a cell thus
+* saving time and operations to perform.
+* This is an improved version of SolveA.
+*
+* @param board a partial or empty Board Struct to find its solution
+* @param row the starting row position - used for the recursion. Must begin with 0.
+* @param col the starting column position, same as row. Must begin with 0.
+*
+* @return true if solution was found. False otherwise.
+*****************************************************************************/
+bool solveB(Board *b, int row, int col);
+
+
+/*****************************************************************************
+* startChild starts an algorithm solution
+*
+* @param board a partial or empty Board Struct to find its solution
+* @param solution choose between the 2 possible solutions:
+*  SOLUTION_A - backtracking algorithm
+*  SOLUTION_B - markup-improved backtracking algorithm
+*
+* @return true if algorithm chosen returned true. False otherwise.
+*****************************************************************************/
+bool startChild(Board* b, SOLUTION s);
